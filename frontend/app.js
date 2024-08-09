16442a623d6b647843d493cebbc336a8fa0a78a4
Levnaic <srdjanlevnaic@gmail.com>
2024-08-09 02:53:23 +0200

    rad na frontu zeza ubacivanje cors-a pogledati sa dokerom

---
+++ frontend/app.js
@@ -0,0 +1,31 @@
+// ee
+function ee() {
+  document.body.style.backgroundImage = "url('./img/ee.jpg')";
+
+  setTimeout(() => {
+    document.body.style.backgroundImage = "";
+  }, 70);
+}
+
+intervalEe = setInterval(ee, 300000);
+
+// form submit
+document.getElementById("movie-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const title = document.getElementById("title").value;
+
+  fetch("http://localhost:5000/submit", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ title: title }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      document.getElementById("response").innerText = data.message;
+      document.getElementById("titleInput").value = ""; // Clear the input field
+    })
+    .catch((error) => console.error("Error:", error));
+});
